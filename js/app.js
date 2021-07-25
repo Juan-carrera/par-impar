04279deb4269c5e44733b0e9ac30cdc0a4c3c080
juan carrera <jacc_1297@hotmail.com>
2021-07-25 18:57:17 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+const d = document;
+
+const form = d.querySelector(".form");
+const resp = d.querySelector(".response");
+
+
+form.addEventListener("submit", validarNumero);
+
+
+function validarNumero(e){
+
+    // PreventDefault evita que el navegador realice la acción por default
+    e.preventDefault();
+    
+    //se obtiene,guarda y castea  el valor del input en una variable
+    let valorInput = parseInt(e.target.number.value);
+
+    //multiplica por 3 el valor obtenido del input
+    let valorInputMultiplicado = valorInput * 3;
+  
+    if((valorInputMultiplicado % 2) === 0){
+
+        //removemos la clase none de nuestro div en el DOM
+        resp.classList.remove("none");
+
+        //insertamos un parrafo en nuestro div del DOM
+        resp.innerHTML = `<p>El numero ${valorInputMultiplicado} es un resultado par</p>`;
+        
+        //limpiamos el input despues de 1 segundo
+        setTimeout(() => { 
+          
+            form.reset();
+
+            //agregampos la clase none a nuestro div en el DOM
+            setTimeout(() => {
+                resp.classList.add("none");
+            }, 3000);
+            
+        }, 1000);
+        
+
+        
+    } else{
+
+        resp.classList.remove("none");
+        resp.innerHTML = `<p>El numero ${valorInputMultiplicado} es un resultado impar</p>`;
+        
+        setTimeout(() => { 
+          
+            form.reset();
+
+            setTimeout(() => {
+                resp.classList.add("none");
+            }, 3000);
+            
+        }, 1000);
+      
+        
+    }
+}
